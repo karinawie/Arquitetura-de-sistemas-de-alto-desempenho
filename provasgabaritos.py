5d776df0e18962c3f1254f937b367c54c754b1e8
Karina <karinawie@users.noreply.github.com>
2020-06-30 19:17:09 -0300

    Update provasgabaritos.py

--- provasgabaritos.py
+++ provasgabaritos.py
@@ -1,27 +1,22 @@
-#script com Scrapy para recolher PDFs. Fontes dos dados no INEP: Provas e gabaritos
+#script com Scrapy para recolher PDFs. Fonte dos dados no INEP: http://inep.gov.br/web/guest/educacao-superior/enade/provas-e-gabaritos
 #este script baixa todos os PDFs de todos os anos 
 
 import scrapy
 from scrapy.selector import Selector
 
-class EnadeSpiderSpider(scrapy.Spider):
+class EnadeSpider(scrapy.Spider):
     name = 'enade_spider'
     start_urls = ['http://inep.gov.br/web/guest/educacao-superior/enade/provas-e-gabaritos']
 
     def parse(self,response):
         base_url = 'http://inep.gov.br/web/guest/educacao-superior/enade/provas-e-gabaritos'
         session_urls = response.xpath('//*[@class="filter__year"]/option/@value').extract()
-        # lista todos os eventos de acordo com o label        
-
         for url in session_urls:
             next_url = base_url.format(url)
             yield scrapy.Request(url=next_url, callback=self.get_pdf)
 
     def get_pdf(self, response):
         pdfs = response.xpath('//div/a[@target="_blank"]/@href').extract() 
-        #//div/a[@target="_blank"]/@href --> mostra todos os links de pdfs
-        # response.xpath('//div/a[@target="_blank"]/@href').extract() --> no terminal 
-
         for pdf in pdfs:
             print(pdf)
             yield scrapy.Request(url=pdf, callback=self.save_pdf)
